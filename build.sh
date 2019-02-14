1f30ec00878c8845642b1a715c4cf98c0d0107bb
Mahyuddin Susanto <udienz@gmail.com>
2019-02-14 12:16:26 +0700

    Add bionic

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 BASE=$CI_PROJECT_DIR
-DISTRO="centos5 centos6 centos7 precise trusty xenial wheezy jessie stretch"
+DISTRO="centos5 centos6 centos7 precise trusty xenial bionic wheezy jessie stretch"
 OUT=.gitlab-ci.yml
 
 cat > $OUT <<EOF
