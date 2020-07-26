af7260ef3b3a8a5a318591d9ee31366445182c5c
Mahyuddin Susanto <udienz@gmail.com>
2020-07-26 19:03:53 +0700

    Remove debian wheezy

--- utils/build.sh
+++ utils/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 BASE=$CI_PROJECT_DIR
-DISTRO="centos6 centos7 precise trusty xenial bionic focal wheezy jessie stretch buster"
+DISTRO="centos6 centos7 precise trusty xenial bionic focal jessie stretch buster"
 OUT=../.gitlab-ci.yml
 
 cat > $OUT <<EOF
