12797a34b02b798f0141e48ee7ce53b67848cb28
Mahyuddin Susanto <udienz@gmail.com>
2020-08-01 23:35:57 +0700

    reproduce citlab-cd

--- utils/build.sh
+++ utils/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 BASE=$CI_PROJECT_DIR
-DISTRO="centos6 centos7 precise trusty xenial bionic focal jessie stretch buster alpine-edge alpine-current"
+DISTRO="centos6 centos7 precise trusty xenial bionic focal jessie stretch buster alpine-edge alpine-latest"
 OUT=../.gitlab-ci.yml
 
 cat > $OUT <<EOF
