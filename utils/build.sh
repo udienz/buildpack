15152e523a084d30e8fc3478614aca5e0b7d0bf2
Mahyuddin Susanto <udienz@gmail.com>
2021-04-12 13:45:39 +0700

    Add centos8
    Signed-off-by: Mahyuddin Susanto <udienz@gmail.com>

--- utils/build.sh
+++ utils/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 BASE=$CI_PROJECT_DIR
-DISTRO="centos6 centos7 precise trusty xenial bionic focal jessie stretch buster alpine-edge alpine-latest"
+DISTRO="centos7 centos8 precise trusty xenial bionic focal jessie stretch buster alpine-edge alpine-latest"
 OUT=../.gitlab-ci.yml
 
 cat > $OUT <<EOF
